456d6f22c4f8fe217068b6a3d17e88efc512c4e3
d.ihsan maulana <121476322+ihsanmaulana123456789@users.noreply.github.com>
2023-10-03 08:06:34 +0700

    Update Tugas2_TipeData.js

--- Tugas2_TipeData.js
+++ Tugas2_TipeData.js
@@ -1,1 +1,9 @@
+let firstName = "D. Ihsan"; 
+let lastName = "Maulana"; 
+let age = 20; 
+let isMarried = false; 
+
+console.log("Nama Lengkap: " + firstName + " " + lastName);
+console.log("Umur: " + age);
+console.log("Status Pernikahan: " + (isMarried ? "Menikah" : "Belum Menikah"));
 
