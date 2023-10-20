be117b2c2fc1248de6b774eddc2bb319e7bac229
d.ihsan maulana <121476322+ihsnmlna@users.noreply.github.com>
2023-10-20 20:14:20 +0700

    Update Tugas_Sesi3.js

--- Tugas_Sesi3.js
+++ Tugas_Sesi3.js
@@ -1,50 +1,45 @@
 const restaurant = {
-    name: "Bebek Kaleyo",
-    city: "Tangerang BSD",
-    favoriteDrink: "Ice Thai Tea",
-    favoriteFood: "Bebek Goreng",
+    name: "Nama Restoran",
+    city: "Nama Kota",
+    favoriteDrink: "Minuman Favorit",
+    favoriteFood: "Makanan Favorit",
     isVegan: true
   };
   
-  // Mengecek apakah properti tidak kosong atau null
-  if (
-    restaurant.name !== "" &&
-    restaurant.city !== "" &&
-    restaurant.favoriteDrink !== "" &&
-    restaurant.favoriteFood !== ""
-  ) {
-    console.log(restaurant);
-  } else {
-    console.log("Properti restaurant tidak boleh kosong atau null.");
-  }
-  
-  // Membuat array restaurant
-const restaurants = [
+  const restaurants = [
     {
-      name: "Gacoan",
-      city: "Bogor",
-      favoriteDrink: "Lemon Tea",
-      favoriteFood: "Mie Pedas",
+      name: "Bebek Kaleyo Resto",
+      city: "Tangerang BSD",
+      favoriteDrink: "Ice Thai Tea",
+      favoriteFood: "Bebek Goreng Bumbu Madura",
       isVegan: true
     },
     {
-      name: "Iga Resto",
+      name: "Cerita Nasi Goreng",
       city: "Serpong",
-      favoriteDrink: "Teh Pucuk",
-      favoriteFood: "Sate Iga",
+      favoriteDrink: "Avocado Juice",
+      favoriteFood: "Nasi Goreng Ceria",
       isVegan: false
-    },
+    }
   ];
   
-  // Mengecek apakah properti tidak kosong atau null pada setiap objek di dalam array
-  restaurants.forEach(restaurant => {
-    if (
-      restaurant.name !== "" &&
-      restaurant.city !== "" &&
-      restaurant.favoriteDrink !== "" &&
-      restaurant.favoriteFood !== ""
-    ) {
-      console.log(restaurant);
-    } else {
-      console.log("Properti restaurant tidak boleh kosong")
-    }});
+  // Menampilkan informasi restoran objek
+  console.log("Informasi Restoran Objek:");
+  console.log("Nama Restoran:", restaurant.name);
+  console.log("Kota:", restaurant.city);
+  console.log("Minuman Favorit:", restaurant.favoriteDrink);
+  console.log("Makanan Favorit:", restaurant.favoriteFood);
+  console.log("Vegan:", restaurant.isVegan ? "Ya" : "Tidak");
+  
+  // Menampilkan informasi restoran dari array
+  console.log("\nInformasi Restoran Array:");
+  restaurants.forEach(function(restaurant, index) {
+    console.log("Restoran ke-" + (index + 1));
+    console.log("Nama Restoran:", restaurant.name);
+    console.log("Kota:", restaurant.city);
+    console.log("Minuman Favorit:", restaurant.favoriteDrink);
+    console.log("Makanan Favorit:", restaurant.favoriteFood);
+    console.log("Vegan:", restaurant.isVegan ? "Ya" : "Tidak");
+    console.log("\n");
+  });
+  
