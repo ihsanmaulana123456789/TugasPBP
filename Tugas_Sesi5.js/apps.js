c778407b424eb2b03c4423b439d5aff387d73d26
d.ihsan maulana <121476322+ihsnmlna@users.noreply.github.com>
2023-11-09 10:24:44 +0700

    Create apps.js

---
+++ Tugas_Sesi5.js/apps.js
@@ -0,0 +1,9 @@
+const fullName= require('./module1');
+const myAge= require('./module2');
+const myHobby= require('./module3');
+const myWork= require('./module4');
+
+console.log(fullName('D. Ihsan Maulana'));
+console.log(myAge('20 tahun'));
+console.log(myHobby('bermain musik, badminton dan berenang'));
+console.log(myWork('Web developer'))
